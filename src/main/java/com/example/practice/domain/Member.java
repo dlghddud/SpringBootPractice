0a1486bd99cf34d18f217c4618c75aa0cdf87b11
이홍영영 <hyhy7012@naver.com>
2025-07-25 00:36:26 +0900

    í회원 도메인과 리포지토리 생성

---
+++ src/main/java/com/example/practice/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.practice.domain;
+
+public class Member {
+    public String id;
+    public String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/practice/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.example.practice.repository;
+
+import com.example.practice.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(String id);
+    // Optional은 값이 존재할 수도 존재하지 않을 수도 있다. NullPointerException 예외를 방지할 수 있고, 코드의 안정성을 높이며 가독성을 향상시킨다.
+    Optional<Member> findByName(String Name);
+    List<Member> findAll();
+}
